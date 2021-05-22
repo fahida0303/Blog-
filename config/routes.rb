a474d73b94408ccae60b540572b32f76c431b7e8
Fahida rios <fahidarios@gmail.com>
2021-05-22 11:11:48 -0500

    agregue un articulos y con el El controlador y las vistas de un recurso

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/index'
+  get 'pages', to: 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'about', to: 'pages#about'
+  get 'Home', to: 'about#home'
+  resources :articles
 end
