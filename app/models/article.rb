ad8813489c7f750094bf7fc063ef45275d4bb1b3
Fahida rios <fahidarios@gmail.com>
2021-05-27 18:02:53 -0500

    realcion entre el articulo  y el usuario

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ActiveRecord::Base
+    
+    belongs_to :user
     validates :title, presence: true,  length: {minimum: 3, maximum: 50}
     validates :descriptio,  presence: true, length: {minimum: 10, maximum: 300}
+    validates :user_id, presence: true
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_27_225200) do
+ActiveRecord::Schema.define(version: 2021_05_27_225916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_05_27_225200) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "descriptio"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
