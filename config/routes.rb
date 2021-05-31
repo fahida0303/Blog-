39c9bc5ccf35a955ba719e76f3bce1d0a2f833c4
Fahida rios <fahidarios@gmail.com>
2021-05-31 16:49:57 -0500

    registro y cierre de secion

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+delete 'logout', to: 'sessions#destroy'
 end
